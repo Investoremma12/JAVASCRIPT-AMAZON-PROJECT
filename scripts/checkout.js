4ccb8b7a8c94e0533465ed2a8fd80d91feb23f61
emmanuel <onyibeemmanuel621@gmail.com>
2026-01-17 06:31:49 +0100

    creatr oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import isSatSun from './utils/datePractice.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-
+import '../data/cart-oop.js';
 renderOrderSummary();
 renderPaymentSummary();
 renderCheckoutHeader();
