59ca9ee4c3de5e70132d9c84d4d30b3e20c46636
emmanuel <onyibeemmanuel621@gmail.com>
2026-01-18 22:11:41 +0100

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,8 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import isSatSun from './utils/datePractice.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 // import '../data/cart-class.js';
+import '../data/backend.js';
+
 renderOrderSummary();
 renderPaymentSummary();
 renderCheckoutHeader();
